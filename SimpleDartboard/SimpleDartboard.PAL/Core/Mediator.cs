22ad2ed9d8ca2db8ab32edb44101e2d532624dd3
Maximilian Kraft <max.kraft@artec.de>
2019-09-28 16:39:57 +0200

    Should add Mediator for communication between ViewModels
    will fix #5

---
+++ SimpleDartboard/SimpleDartboard.PAL/Core/Mediator.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace SimpleDartboard.PAL.Core
+{
+    public class Mediator
+    {
+        static IDictionary<MessageType, List<Action<object>>> _actionDictionary =
+            new Dictionary<MessageType, List<Action<object>>>();
+
+        public static void Register(MessageType messageType, Action<object> callback)
+        {
+            if (!_actionDictionary.ContainsKey(messageType))
+            {
+                var actions = new List<Action<object>>();
+                actions.Add(callback);
+                _actionDictionary.Add(messageType, actions);
+                return;
+            }
+
+            if (_actionDictionary[messageType].Exists(x => x.Method.ToString() == callback.Method.ToString()))
+            {
+                _actionDictionary[messageType].Add(callback);
+            }
+        }
+
+        public static void Unregister(MessageType messageType, Action<object> callback)
+        {
+            if (_actionDictionary.ContainsKey(messageType))
+                _actionDictionary[messageType].Remove(callback);
+        }
+
+        public static void NotifyColleagues(MessageType messageType, object argument)
+        {
+            if (!_actionDictionary.ContainsKey(messageType)) return;
+            foreach (var callback in _actionDictionary[messageType])
+                callback(argument);
+        }
+    }
+}
