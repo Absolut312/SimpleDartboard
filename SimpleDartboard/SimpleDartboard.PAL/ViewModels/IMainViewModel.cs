df69dc597bbff553470cbd353991d9b60e4a8338
Maximilian Kraft <max.kraft@artec.de>
2019-09-28 18:04:21 +0200

    Should add routing for MainViewModel Content

--- SimpleDartboard/SimpleDartboard.PAL/ViewModels/IMainViewModel.cs
+++ SimpleDartboard/SimpleDartboard.PAL/ViewModels/IMainViewModel.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace SimpleDartboard.PAL.ViewModels
 {
     public interface IMainViewModel
     {
         INavigationBarViewModel NavigationBarViewModel { get; set; }
+        IContentViewModel ContentViewModel { get; set; }
     }
 }
