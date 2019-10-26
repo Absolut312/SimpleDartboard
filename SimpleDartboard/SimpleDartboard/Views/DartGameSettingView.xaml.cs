48f93369028bea5db4f6aba918b62dbe06c1375e
Maximilian Kraft <max.kraft@artec.de>
2019-10-26 16:05:04 +0200

    readd missing files

---
+++ SimpleDartboard/SimpleDartboard/Views/DartGameSettingView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SimpleDartboard.Views
+{
+    public partial class DartGameSettingView : UserControl
+    {
+        public DartGameSettingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SimpleDartboard/SimpleDartboard/Views/DartGameView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SimpleDartboard.Views
+{
+    public partial class DartGameView : UserControl
+    {
+        public DartGameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
